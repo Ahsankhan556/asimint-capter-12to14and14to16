bc47cc05ffe01facde20124cce02a2bb3bb2b5b4
Ahsankhan556 <ak5589667@gmail.com>
2023-09-13 11:51:17 +0845

    asimint chapter12to13and14to16

---
+++ app.js
@@ -0,0 +1,150 @@
+                                    //? chapter 12 to 13 
+                                          //! Qno 1
+
+// var char=prompt("Enter value")
+// if (char >= "A" && char <="Z"){
+//    alert(char + " is uppercase")
+// }
+// else if (char >= "a" && char <= "z") {
+//    alert(char + " is Lowercase");
+// }
+// else if (char >= "0" && char <= "9") {
+//    alert(char + " is a Number");
+// }
+// else{
+//    alert("The character is neither number noralphabet")
+// } 
+
+                                               // ! Q no 2
+
+// var a = parseInt(prompt("value"));
+// var num = Number.isInteger(a)
+// alert(num);
+
+                                             // ! Q no 3
+
+// var number = parseInt(prompt("Enter a number: "));
+// if (number > 0) {
+//     alert("The number is positive");
+// }
+// else if (number == 0) {
+//   alert("The number is zero");
+// }
+// else {
+//      alert("The number is negative");
+// }                                         
+
+                                             // ! Q no 4\
+
+
+
+
+
+
+
+
+
+
+
+                                          //     ! Q no 5
+
+// var usarInput = prompt("Enter your password:")
+// var pasword = 123456
+// if (usarInput == pasword){
+//       alert("Correct Password");
+// }
+// // console.log(usarInput);
+//  else if (usarInput == "" || !usarInput.trim())
+// {alert("enter your pasword")}
+// else{
+//       alert("rong password!")
+// }
+
+                                                // ! Q no 6 
+
+// //    var greeting;
+
+// // var usarInput = prompt("hour:");
+// var hour = 13;
+// if (hour < 18) {
+//   alert ("Good day");
+// } else {
+//   alert ("Good evening");
+// }
+                                            
+                                          //    ! Q no 7
+// // Prompt the user for input
+// var userInput = prompt("Enter the time in 24-hour format (e.g., 1900):");
+// var time = parseInt(userInput);
+
+// if (isNaN(time)) {
+//   alert("Please enter a valid time.");
+// } else {
+//   if (time >= 0 && time < 1200) {
+//     alert("Good morning!");
+//   } else if (time >= 1200 && time < 1700) {
+//     alert("Good afternoon!");
+//   } else if (time >= 1700 && time < 2100) {
+//     alert("Good evening!");
+//   } else if (time >= 2100 && time <= 2359) {
+//     alert("Good night!");
+//   } else {
+//     alert("Invalid time input. Please enter a time between 0000 and 2359.");
+//   }
+// }
+                      
+                        // ?chapter no 14 and 16
+                              // !  Q no 1
+
+// var studentNames = [];
+
+                              // ! Q no 2
+
+// var studentList = {
+//   names: []
+// };
+
+                              // !Qno 3
+
+// var fruits = ["apple", "banana", "cherry", "date"];
+
+
+                              //  ! Qno 4
+
+// var numbers = [1, 2, 3, 4, 5];
+
+                              //! Q no 5
+                              
+                              
+// var fruits = [true, false, true, false];
+
+                             // ! Q no 6
+
+// var mixedArray = ["apple", 123, true, "banana", false];
+
+                              // ! Q no 7
+
+// var listedQualification = [" ,1) SSC </br>","2) HSC </br>" ," 3) BSC </br>"," 4) BS </br>"," 5) BCOM </br>"," 6) MS </br>"," 7) M.PHIL </br>"," 8) PHD </br>"] 
+// document.write("<h1>Qualification</h1>")     
+// document.write(listedQualification )
+
+                              // ! Qno 8
+// var studentNames = ["John", "Alice", "Bob"];
+
+// var studentScores = [420, 380, 450];
+
+// var totalMarks = 500;
+
+// var percentages = [];
+// for (let i = 0; i < studentNames.length; i++) {
+//     let percentage = (studentScores[i] / totalMarks) * 100;
+//     percentages.push(percentage.toFixed(2)); 
+// }
+
+// for (let i = 0; i < studentNames.length; i++) {
+//    document.write(`Student: ${studentNames[i]}`);
+//    document.write(`Score: ${studentScores[i]}`);
+//    document.write(`Percentage: ${percentages[i]}%`);
+//    document.write("------------");
+// }
+
